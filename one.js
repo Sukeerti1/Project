5368a5f71e2132b39067d821c68be7663826e13a
Sukeerti1 <sukeerti17@gmail.com>
2022-06-19 20:08:24 +0530

    practice que

--- one.js
+++ one.js
@@ -1,8 +1,129 @@
-var num = 234557;
+//check if no is divisible by/multiple of  7
+/*var num = 42;
+(num % 7 == 0) ? console.log("Divisible by 7") : console.log("Not divible by 7") */
+
+//check if no is positive or not
+/*var num = -2
+if(num > 0){
+    console.log("Positive number")
+}
+else if(num == 0){
+    console.log("Neither positive nor negative")
+}
+else{
+    console.log("Negative number")
+} */
+
+//check if unit digit is 4
+/*var num = 234557;
 if(num % 10 ==4){
     console.log("Unit digit is 4");
 }
 else
 { console.log("Unit digit is not 4");
-}
+} */
 
+// check if number is 3 digit number
+/*var a = 433
+if(( a > 99)  && (a < 1000)){
+    console.log("3 digit number")
+}
+else{
+    console.log("Not a 3 digit number")
+} */
+
+
+// print greater number/ lesser number
+/*var a = 5, b = 41;
+if (a > b){
+    console.log( a ," is greater number")
+}
+else{
+    console.log( b ," is greater number")
+} */
+
+// print given 3 numbers in ascending order/ descending order
+/*var a = 5, b = 10, c = 15;
+if (a>b && a>c){
+    if (b>=c)
+    console.log(a, ">", b, ">", c)
+    else
+    console.log(a, ">", c,">", b)
+}
+else if (b>a && b>c){
+    if (a>=c)
+    console.log(b, ">", a, ">", c)
+    else
+    console.log(b, ">", c, ">", a)
+}
+else if (c>a && c>b){
+    if (a>=b)
+    console.log(c, ">", a, ">", b)
+    else
+    console.log(c, ">", b, ">", a)
+} */
+
+//print 1 to 10
+/*let i=1;
+while (i<11){
+    console.log(i)
+    i++
+} */
+
+//print 15 to 10
+/*for(let i=15;i>9;i--){
+    console.log(i)
+} */
+
+//print 1st 10 even numbers using while loop
+/*let i=0
+while(i<20) {
+    if(i % 2 ==0)
+    console.log(i)
+    i++
+} */
+
+//print 1st 10 odd numbers using for loop
+/*for ( let i=0;i<20;i++){
+    if(i%2 == 1)
+    console.log(i)
+} */
+
+//print multiple of 4
+/*for (let i=1;i<=10;i++){
+    console.log("4*",i,"=",4*i)
+} */
+
+//print multiple of 6
+/*let i=1
+while(i<=10){
+    console.log("6*",i,"=",6*i)
+    i++
+} */
+
+//factor of 24
+/*let i=1
+while(i<=24){
+    if(24 % i == 0)
+    console.log(i)
+    i++
+} */
+
+//reverse a number
+/*var num = 789, reverse = 0, remainder;
+while(num>0){
+    remainder = num % 10;
+    reverse = reverse * 10 + remainder;
+    num = Math.floor(num / 10);
+}
+console.log(reverse); */
+
+//read 5 numbers and print only even numbers
+/*var i1,i2,i3,i4,i5;
+var evens;
+i1=parseInt(i1);
+i2=parseInt(i2);
+i3=parseInt(i3);
+i4=parseInt(i4);
+i5=parseInt(i5);
+ */
